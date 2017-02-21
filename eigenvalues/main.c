f8f4e7cde25496044b8592817ce76f76af7875bf
Aqqaluk Sørensen <aqqaluksorensen@Aqqaluks-Air.lan>
2017-02-21 22:34:28 +0100

    Eigen

--- eigenvalues/main.c
+++ eigenvalues/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <gsl/gsl_eigen.h>
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+
+const double pi = 3.14159265358979323844;
 
 int main(void)
 {
-	/* copy from the hint */
+	/* copy from the hints */
+	/* Hamiltonian matrix  H′*/
 	int n=20;
 	double s=1/(n+1);
 
@@ -17,5 +22,22 @@ int main(void)
 	
 	gsl_matrix_set(H,n-1,n-1,-2);
 	gsl_matrix_scale(H,-1/s/s);
+	
+	/* Diagonalization of the matrix */
+	gsl_eigen_symmv_workspace* w =  gsl_eigen_symmv_alloc (n);
+	gsl_vector* eval = gsl_vector_alloc(n);
+	gsl_matrix* evec = gsl_matrix_calloc(n,n);
+	gsl_eigen_symmv(H,eval,evec,w);
+	
+	/* Sort the eigenvalues and eigenvectors. */
+	gsl_eigen_symmv_sort(eval,evec,GSL_EIGEN_SORT_VAL_ASC);
+	
+	/* Check that your energies are correct */
+	fprintf (stderr, "i   exact   calculated\n");
+	for (int k=0; k < n/3; k++){
+	    double exact = pi*pi*(k+1)*(k+1);
+	    double calculated = gsl_vector_get(eval,k);
+	    fprintf (stderr, "%i   %g   %g\n", k, exact, calculated);
+	}
 	return 0;
 }
